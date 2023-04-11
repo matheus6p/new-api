00970b30df28977af536b25f99c90a119e17a6da
matheus6p <mhpaes97@gmail.com>
2023-04-11 08:35:29 -0300

    fix cors error

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ db.once("open", () =>
 );
 
 const server = fastify();
-server.register(cors);
+server.register(cors, {origin: true});
 server.register(itemRoutes);
 
 server.listen({ port: 3333 }, (err, address) => {
