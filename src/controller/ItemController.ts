ccaa2b875da02533165624e77c2dfb4de546a97e
matheus6p <mhpaes97@gmail.com>
2023-04-11 11:16:07 -0300

    config cors

--- src/controller/ItemController.ts
+++ src/controller/ItemController.ts
@@ -10,8 +10,7 @@ export async function getAllItems(
 ): Promise<ItemModel[]> {
   try {
     const items = await itemService.getAllItems();
-    res.header('Access-Control-Allow-Origin', '*');
-    return res.status(200).send(items);
+    return res.status(200).send(items).header('Access-Control-Allow-Origin', '*');
   } catch (err: any) {
     return res.status(500).send({ message: "Cannot load content", error: err.message });
   }
