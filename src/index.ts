a2a80e4c9cb874efad54d48bfb0d05669de57513
matheus6p <mhpaes97@gmail.com>
2023-04-11 11:33:28 -0300

    cors config

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,17 @@ db.once("open", () =>
 );
 
 const server = fastify();
-server.register(fastifyCors);
+server.register(fastifyCors, {
+  origin: true,
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Accept",
+    "Content-Type",
+    "Authorization",
+  ],
+  methods: ["GET", "PUT", "PATCH", "POST", "DELETE"],
+});
 server.register(itemRoutes);
 
 server.listen({ port: 3333 }, (err, address) => {
