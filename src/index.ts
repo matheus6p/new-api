f649d284b1e7e90445556a090407c346f1702737
Matheus Paes <mhpaes97@gmail.com>
2023-04-12 18:55:51 -0300

    CORS

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = fastify();
 app.register(express);
 app.use(cors, {
   origin: "https://shop-list-kappa.vercel.app",
-  methods: ["GET", "PUT", "POST", "DELETE"],
+  methods: "GET, PUT, POST, UPDATE, DELETE",
   allowedHeaders: ["Content-Type"],
   exposedHeaders: ["Content-Disposition"],
 });
